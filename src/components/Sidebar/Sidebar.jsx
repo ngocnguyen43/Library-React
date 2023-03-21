9d51524065974d80f881da9431b83f961a3312cd
Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-03-21 23:30:54 +0700

    #3 sidebar base

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import './Sidebar.scss';
+export const Sidebar = () => {
+	return (
+		<div className="sidebar-container">
+			<div className="sidebar-content">
+				<a>Logo</a>
+				<a>Dashboard</a>
+				<a>User</a>
+				<a>Book</a>
+			</div>
+		</div>
+	);
+};
